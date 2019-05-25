93139bbd8a9aeb2a3371d590c0ef217e7eff4e5d
Talha <talhasiddiqui922000@gmail.com>
2019-05-25 20:30:13 +0500

    Added more comments

--- Binary Search/binary search/binary search/Module1.vb
+++ Binary Search/binary search/binary search/Module1.vb
@@ -33,7 +33,7 @@
         Dim MID As Integer = 0
 
         While (UB >= LB And isFound = False)
-            MID = (UB + LB) / 2 'As the data types are integers so result is also an integer instead of decimal or we can use \'
+            MID = (UB + LB) / 2 'As the data types are integers so result is also an integer instead of decimal or we can use \' 'NOTE: Use DIV in Psedocode
             If myArr(MID) = searchVal Then
                 Console.WriteLine("Found your search value " & searchVal & " at location: " & MID)
                 isFound = True
@@ -56,7 +56,7 @@
 
     Function recursiveBinarySearch(ByVal LB As Integer, ByVal UB As Integer, ByRef myArr() As Char, ByVal searchVal As Char) As Integer
         Dim MID As Integer = 0
-        MID = (UB + LB) / 2 'As the data types are integers so result is also an integer instead of decimal or we can use \'
+        MID = (UB + LB) / 2 'As the data types are integers so result is also an integer instead of decimal or we can use \' 'NOTE: Use DIV in Psedocode
         If (LB > UB) Then
             Return -1 ' MEANS NOT FOUND
         Else
