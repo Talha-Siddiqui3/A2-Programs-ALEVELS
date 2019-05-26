123ba8f8ac3715ffbce06694cd16180b8054b61a
Talha <talhasiddiqui922000@gmail.com>
2019-05-26 18:02:35 +0500

    Added Exception Handling Program

---
+++ Exception Handling IMP/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim a As Integer
+        Dim valid = False
+        Do
+            Console.WriteLine("Enter a number:")
+            Try
+                a = Console.ReadLine()
+                valid = True
+            Catch
+                Console.WriteLine("Wrong input" & vbCrLf & "Please enter a valid number")
+                valid = False
+            End Try
+        Loop Until valid
+        Console.ReadKey()
+    End Sub
+
+End Module
